4f9a8da9ea5f6c7dc0721d35695d6ff24bff69e7
Ugesh Kumar KS <ugesh262000@gmail.com>
2023-04-19 15:22:50 +0530

    Intial commit

--- Expressions.sh
+++ Expressions.sh
@@ -2,3 +2,7 @@ read -p "Enter the value of a: " a
 read -p "Enter the value of b: " b
 read -p "Enter the value of c: " c
 
+sum=$(awk "BEGIN {print $a + $b * $c}")
+
+echo " The Sum Of Numbers " $sum
+
